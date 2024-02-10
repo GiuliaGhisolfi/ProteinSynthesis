c94952897ddebe206687fdbe5f7f85cb4247fd1a
Giu <giuliaghisolfi99@gmail.com>
2024-02-10 01:45:18 +0100

    transcription

--- src/protein_synthesis.py
+++ src/protein_synthesis.py
@@ -1,3 +1,4 @@
+import simpy
 import json
 from src.transcription import Transcription
 from src.translation import Translation
@@ -8,11 +9,30 @@ PEPTIDES_PATH = DATA_PATH + 'peptides.json'
 
 class ProteinSynthesis:
     def __init__(self, dna):
-        self.dna = dna
+        self.dna = dna # template strand (3' to 5' direction)
         self.codons = json.load(open(CODONS_PATH))
         self.peptides = json.load(open(PEPTIDES_PATH))
+
+        env = simpy.Environment
+        #pipeline = BroadcastPipe(env)
 
         self.protein = self.synthesize_protein()
 
     def synthesize_protein(self):
-        pass
+        self.transcription = Transcription(
+            intron_sequences_list=[], # TODO
+            editing_sites_dict={}, #TODO
+            promoters_sequence_list=['TATAAAA', 'TATAAAT', 'TATATAA', 'TATATAT'], # TATA box
+            terminator_sequence=['UAA', 'UAG', 'UGA'] # TODO: valutare se cambiare stop codons
+            )
+        self.mrna = self.transcription.transcript(self.dna)
+        #TODO: Implement Translation
+    
+    def get_dna(self):
+        return self.dna
+    
+    def get_mrna(self):
+        return self.mrna
+    
+    def get_protein(self):
+        return self.protein
--- src/transcription.py
+++ src/transcription.py
@@ -1,43 +1,92 @@
 def splicing(rna_sequence, intron_sequences):
-    rna_sequence = [rna_sequence.replace(intron_sequence, '') for intron_sequence in intron_sequences]
-
-    return ''.join(rna_sequence)
+    for intron_sequence in intron_sequences:
+        rna_sequence = rna_sequence.replace(intron_sequence, '')
+    return rna_sequence
 
 def editing(rna_sequence, editing_sites_dict):
-    rna_sequence = [rna_sequence.replace(key, value) for key, value in editing_sites_dict.items()]
-
-    return ''.join(rna_sequence)
+    for editing_site in editing_sites_dict:
+        rna_sequence = rna_sequence.replace(editing_site, editing_sites_dict[editing_site])
+    return rna_sequence
 
 def capping(rna_sequence):
-    return 'CH3GPPP{}'.format(rna_sequence) # Add 5'-methyl cap
+    return 'CH3GPPP-{}'.format(rna_sequence) # Add 5'-methyl cap
 
 def polyadenylation(rna_sequence):
-    return '{}AAAA'.format(rna_sequence) # Add PolyA tail
+    return '{}-AAAA'.format(rna_sequence) # Add PolyA tail
 
-BASE_COMPLEMENT = {
+BASE_COMPLEMENT_DNA2RNA = {
     'A': 'U', 
     'T': 'A', 
     'C': 'G', 
     'G': 'C'
 }
+BASE_COMPLEMENT_RNA2DNA = {
+    'U': 'A', 
+    'A': 'T', 
+    'G': 'C', 
+    'C': 'G'
+}
+RNA_POLYMERASE_ERROR_RATE = 10e-4 # 1 error per 10^4 nucleotides
 
 class Transcription():
 
-    def __init__(self, intron_sequences_list, editing_sites_dict):
+    def __init__(self, intron_sequences_list, editing_sites_dict,
+            promoters_sequence_list, terminator_sequence):
         self.intron_sequences_list = intron_sequences_list
+
         self.editing_sites_dict = editing_sites_dict
+        self.editing_sites_dict = dict(sorted(self.editing_sites_dict.items(), 
+            key=lambda x: len(x[0]), reverse=False)) # sort by length of key
+
+        self.promoters_sequence_list = promoters_sequence_list
+        self.terminator_sequence = terminator_sequence
+        # enzime: RNA polymerase
+
+        # type of RNA
+        # - messenger RNA (mRNA): code for proteins
+        # - transfer RNA (tRNA): transport amino acids to ribosomes
+        # - ribosomal RNA (rRNA): form the core of a ribosome's functional center
+        # - small nuclear RNA (snRNA): splicing of pre-mRNA
+        # - small cajan RNA (scaRNA): modification of snRNA and snoRNA
+        # - small nucleolar RNA (snoRNA): processing and modification of rRNA, tRNA, and snRNA
+        # - microRNA (miRNA): regulate gene expression
+        # - small interfering RNA (siRNA): regulate gene expression
     
+    def find_promoter(self, dna_sequence):
+        # find promoter sequence
+        positions_list = [dna_sequence.find(promoter) for promoter in self.promoters_sequence_list]
+        if positions_list:
+            promoter_posotion = min([pos for pos in positions_list if pos > 0])
+        else:
+            raise ValueError('No promoter found')
+
+        promoter_string = self.promoters_sequence_list[positions_list.index(promoter_posotion)]
+        len_promoter = len(promoter_string)
+
+        return dna_sequence[promoter_posotion+len_promoter:]
+    
+    def find_terminator(self, rna_sequence):
+        # find terminator sequence
+        positions_list = [pos for pos in [rna_sequence.find(terminator) for terminator in 
+            self.terminator_sequence] if pos > 0]
+        terminator_position = -1 if not positions_list else max(positions_list) 
+        
+        return rna_sequence[:terminator_position]
+
     def transcript(self, dna_sequence):
-        rna_sequence = [BASE_COMPLEMENT[base] for base in dna_sequence]
-        rna_sequence = ''.join(rna_sequence)
+        # detect promoter
+        dna_sequence_to_transcript = self.find_promoter(dna_sequence)
+        
+        # transcription initiation
+        messenger_rna_sequence = ''.join([BASE_COMPLEMENT_DNA2RNA[base] for base in dna_sequence_to_transcript])
+        messenger_rna_sequence = self.find_terminator(messenger_rna_sequence)
 
-        rna_sequence = splicing(rna_sequence, self.intron_sequences_list)
+        # transcription elongation
+        messenger_rna_sequence = splicing(messenger_rna_sequence, self.intron_sequences_list)
+        messenger_rna_sequence = editing(messenger_rna_sequence, self.editing_sites_dict)
+        messenger_rna_sequence = capping(messenger_rna_sequence)
+        messenger_rna_sequence = polyadenylation(messenger_rna_sequence)
 
-        rna_sequence = editing(rna_sequence, self.editing_sites_dict)
-
-        rna_sequence = capping(rna_sequence)
-        rna_sequence = polyadenylation(rna_sequence)
-
-        return rna_sequence
+        return messenger_rna_sequence
     
     
