2a7865bafef25665c705e62616dd6ead421c7374
Giu <giuliaghisolfi99@gmail.com>
2024-02-22 10:12:41 +0100

    process example works!!

--- utils/examples/process.py
+++ utils/examples/process.py
@@ -1,36 +1,35 @@
 import simpy
 import random
 
-def process(env, sentence, t_inter, sentence_number):
-    words = sentence.split()
-    for word in words:
-        word = ''.join([letter for letter in word if letter not in 'aeiou'])
+def process(env, sentence, t_inter, sentence_number, machines):
+    with machines.request() as request:
+        print(f'Sentence {sentence_number} requesting at {env.now}')
+        yield request
+        print(f'Sentence {sentence_number} got resource at {env.now}')
 
-    yield env.timeout(t_inter*3)
-    print(f'Sentence {sentence_number} splited at {env.now}')
-    
+        words = sentence.split()
+        for word in words:
+            word = ''.join([letter for letter in word if letter not in 'aeiou'])
+
+        yield env.timeout(t_inter*3)
+        print(f'Sentence {sentence_number} splited at {env.now}')
+        machines.release(request)
 
 def setup(env, sentences, num_machines, t_inter):
     machines = simpy.Resource(env, num_machines)
-    sentence_number = 0
+    queue = []
 
-    while True:
-        sentence = sentences[sentence_number]
+    for sentence_number, sentence in enumerate(sentences):
+        queue.append(env.process(process(env, sentence, t_inter, sentence_number, machines)))
+        yield env.timeout(1)
 
-        while sentence_number < len(sentences):
-            with machines.request() as request:
-                print(f'Sentece {sentence_number} requested at {env.now}')
-                yield request
-                print(f'Sentece {sentence_number} got resource at {env.now}')
-
-                env.process(process(env, sentence, t_inter, sentence_number))
-                sentence_number += 1
-                yield env.timeout(t_inter)
+    while queue:
+        yield queue.pop(0)
 
 # Setup and start the simulation
 env = simpy.Environment()
 sentences = ['Hello word', 'World cosa caso casa', 'Ciao come stai', 'Io sto bene', 'Tu come stai', 'Io sto'
     'bene', 'Tu come stai', 'Io sto bene', 'Tu come stai', 'Io sto bene', 'Tu come stai', 'Io sto bene', 'Tu come stai']
 random.seed(42)
-env.process(setup(env, sentences, num_machines=1, t_inter=1))
+env.process(setup(env, sentences, num_machines=2, t_inter=1))
 env.run(until=5)
