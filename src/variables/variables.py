a1a4d89e83d4441e303133141a2ab0324c5d5bbb
Giu <giuliaghisolfi99@gmail.com>
2024-03-02 15:54:59 +0100

    documentation

--- src/variables/variables.py
+++ src/variables/variables.py
@@ -1,5 +1,59 @@
 
 class EucaryotesCellVariables:
+    """
+    Class to store the variables of the eucaryotes cell simulation.
+
+    Attributes
+    ----------
+    dna_sequence : str
+        The DNA
+    sequence_count : int
+        Counter of the DNA sequence currently being processed
+    dna_sequences_to_transcript_list : list
+        List of DNA sequences to transcript to mRNA, each element found in the DNA sequencebetween two promoters
+    promoters_count : int
+        Number of the promoters found in the DNA sequence
+    poly_adenine_tail_len : list
+        Length of the poly-adenine tail for each mRNA sequence
+    mrna_degradation_rate : list
+        Rate of mRNA degradation for each mRNA sequence, initially set to 1e-4, 
+        it increases with the number of ribosomes attached to the mRNA
+    request_start_process_time : float
+        Time when the simulation was requested to start
+    start_process_time : float
+        Time when the simulation started
+    self.start_transcription_time: list
+        Time when the transcription of each mRNA sequence started
+    start_translation_time: list
+        Time when the translation of each mRNA sequence started
+    end_translation_time: list
+        Time when the translation of each mRNA sequence ended
+    end_process_time: float
+        Time when the simulation ended
+    promoters_box: list
+        List of promoters found in the DNA sequence
+    mrna_sequences_list: list
+        List of mature mRNA sequences
+    proteins_list: list
+        List of polypeptides chains
+    proteins_extended_name_list: list
+        List of polypeptides chains extended name
+    proteins_sintetized: list
+        List of number of polypeptides chains sintetized for each mRNA sequence
+    transcription_queue: list
+        List of simpy processes for the transcription, 
+        this list is used to store and relase the simpy processes for each mRNA sequence
+    complement_base_queue_dict: dict
+        Dictionary of list of simpy processes for each complement base,
+        this dictionary is used to store and relase the simpy processes
+    polyadenylation_queue: list
+        List of simpy processes for the polyadenylation, 
+        this list is used to store and relase the simpy processes for each mRNA sequence
+    translation_queue: list
+        List of simpy processes for the translation, 
+        this list is used to store and relase the simpy processes for each mRNA sequence
+
+    """
     def __init__(self):
         # input variables
         self.dna_sequence = None # template strand (3' to 5' direction)
